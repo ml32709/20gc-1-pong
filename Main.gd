82fa5cfa24ec68952edb9802c7b710b3f309961a
Matt <matthewlowstx@gmail.com>
2023-12-11 12:41:55 -0700

    2 players mode

--- Main.gd
+++ Main.gd
@@ -17,3 +17,7 @@ func _on_play_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_pvp_pressed():
+	get_tree().change_scene_to_file("res://pvp_arena.tscn")
--- Player/Player2.gd
+++ Player/Player2.gd
@@ -3,9 +3,8 @@ extends CharacterBody2D
 var SPEED = 300
 
 func _physics_process(delta):
-	position.x = 100
 	# Get input directions up or down
-	var direction = Input.get_axis("p1_up", "p1_down")
+	var direction = Input.get_axis("ui_up", "ui_down")
 
 	velocity.y = direction * SPEED
 	
