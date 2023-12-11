b602af693fe5b75a8b85f5bfd6149b0f94091c43
Matt <matthewlowstx@gmail.com>
2023-12-11 11:37:11 -0700

    Ball and foe movement

--- Ball/ball.gd
+++ Ball/ball.gd
@@ -1,20 +1,26 @@
 extends CharacterBody2D
 
 var SPEED = 300.0
+var xDirection = -1
+var yDirection = -1
+var contestantPoint = 0
 
 func _physics_process(delta):
-
+	velocity.x = xDirection * SPEED
+	velocity.y = yDirection * SPEED
 	move_and_slide()
 
 func _on_bump_detection_body_entered(body):
-	if body.name == "player":
-		# Flip x direction
-		pass
-		
-	if body.name == "foe":
-		#Flip x direction
-		pass
-		
-	if body.name == "PointWall":
-		# Need to distinguish between left and right wall, then change score accordingly
-		pass
+	if body.name == "PointWall" || body.name == "PointWall2":
+		print("point")
+		SPEED = 0
+		if xDirection == -1:
+			contestantPoint = 1
+		if xDirection == 1:
+			contestantPoint = -1
+	else:
+		SPEED += 20
+		xDirection *= -1
+
+func _on_wall_bump_detection_body_entered(body):
+	yDirection *= -1
--- Foe/foe.gd
+++ Foe/foe.gd
@@ -1,7 +1,24 @@
 extends CharacterBody2D
 
 var SPEED = 300.0
+var yDirection = 0
 
 func _physics_process(delta):
-
+	position.x = 1050
+	
+	var arena_node = get_parent()
+	var ball_node = arena_node.get_node("Ball")
+	var ball_position = ball_node.global_position
+	
+	if ball_position.y > position.y:
+		yDirection = 1
+	
+	if ball_position.y < position.y:
+		yDirection = -1
+	
+	if ball_position.y == position.y:
+		yDirection = 0
+		
+	velocity.y = yDirection * SPEED
+		
 	move_and_slide()
