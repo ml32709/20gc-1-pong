3ae67c3d1edecf37a21cd1a3c49688b453287dfa
Matt <matthewlowstx@gmail.com>
2023-12-11 02:05:14 -0700

    removed printing in player script

--- Player/player.gd
+++ Player/player.gd
@@ -5,7 +5,6 @@ var SPEED = 300
 func _physics_process(delta):
 	# Get input directions up or down
 	var direction = Input.get_axis("ui_up", "ui_down")
-	print(direction)
 
 	velocity.y = direction * SPEED
 	
