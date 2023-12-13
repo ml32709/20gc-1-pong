457bcaf18efbf861f2225c83f7c86b99856c5f0d
Matt <matthewlowstx@gmail.com>
2023-12-12 19:20:05 -0700

    fixed edge collision drag for P2 in pvp

--- Player/Player2.gd
+++ Player/Player2.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var SPEED = 300
 
 func _physics_process(delta):
+	position.x = 1050
 	# Get input directions up or down
 	var direction = Input.get_axis("ui_up", "ui_down")
 
