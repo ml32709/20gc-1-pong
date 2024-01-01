33470bb1d10e282db885a4e22c5b942df52b4576
Matt <matthewlowstx@gmail.com>
2024-01-01 05:11:07 -0700

    Title screen logo, all that is left is audio and sfx

--- Main.gd
+++ Main.gd
@@ -3,20 +3,16 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://arena.tscn")
 
-
 func _on_quit_button_pressed():
 	get_tree().quit()
-
 
 func _on_pvp_pressed():
 	get_tree().change_scene_to_file("res://pvp_arena.tscn")
